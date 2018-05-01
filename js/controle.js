b5cdf2ef782239f104f4742ca2e8905feb243096
Pedro Wanderley <pedro.wanderley@ccc.ufcg.edu.br>
2018-05-01 20:53:25 -0300

    ...

--- js/controle.js
+++ js/controle.js
@@ -2,11 +2,13 @@ function escrever(){
   var titulo = document.getElementById("titulo").value;
   var msg = document.getElementById("msg").value;
   var author = document.getElementById("author").value;
+  var id = document.getElementById("Id").value;
+  var senha = document.getElementById("senha").value;
   var obj = {};
   obj.title = titulo;
   obj.msg = msg;
   obj.author = author;
-  obj.id = "pwanderley:28101997";
+  obj.id = id + ":" + senha;
   enviarMsg(obj);
 }
 function enviarMsg(mensagem){
@@ -19,13 +21,11 @@ function enviarMsg(mensagem){
 
 }
 function deletar(){
-  axios({
-    method: 'delete',
-    url: `${'http://150.165.85.16:9900/api/msgs/2'}`,
-    data: null,
-    withCredentials: true,
-    headers: {'Content-Type': 'application/json'},
-    credentials: 'pwanderley:28101997'
-  
-})
+  axios.delete('http://150.165.85.16:9900/api/msgs/2', {
+    headers: {
+      'uid': 'pwanderley',
+      'Content-Type': 'application/json',
+      credentials: 'pwanderley:28101997'
+    }
+  })
 }
