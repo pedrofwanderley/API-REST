dafc57df2cda466689028bfe73e3aed9d6e044ca
pedrofwanderley <pedro.wanderley@ccc.ufcg.edu.br>
2018-05-07 16:34:31 -0300

    delete feito

--- js/controle.js
+++ js/controle.js
@@ -20,12 +20,14 @@ function enviarMsg(mensagem){
 }) 
 
 }
-function deletar(id){
-  axios.delete('http://150.165.85.16:9900/api/msgs/' + id, {
-    headers: {
-      'uid': 'pwanderley',
-      'Content-Type': 'application/json',
-      credentials: 'pwanderley:28101997'
-    }
-  })
+function deletar() {
+  var id = document.getElementById("id_delete").value;
+  var id = document.getElementById("id_delete").value;
+  fetch('http://150.165.85.16:9900/api/msgs'+ "/" + id,{
+    method: "delete",
+    body: JSON.stringify({credentials: 'pwanderley:28101997'})
+    }).then(response => 
+    response.json().then(json => {
+      return json;
+    }))
 }
